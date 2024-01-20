40bc7cb2faa7f329c6e8de37f981e15f46810804
Diego <diego.sfc100@hotmail.com>
2024-01-20 00:50:16 -0300

    Adiciona a classe Category com propriedades para CategoryId, Icon, Title e Type.

---
+++ MoneyNote/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoneyNote.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CaregoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public int Icon { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public int Title { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public int Type { get; set; }
+    }
+}
