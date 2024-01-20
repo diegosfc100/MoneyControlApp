fb401afa07ec5af76371f1f1cecadb3b8e4cdaf9
diego <diego@DESKTOP-BMBK6G4>
2024-01-20 00:24:31 -0300

    Add project files.

---
+++ MoneyNote/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoneyNote.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
