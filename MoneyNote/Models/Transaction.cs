8b24e31bc48e16584382e8529cdbb0aa21f688ab
Diego <diego.sfc100@hotmail.com>
2024-01-23 15:18:08 -0300

    Adiciona a classe 'Transaction' com propriedades TransactionId, Amount, Note e Date."

---
+++ MoneyNote/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyNote.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //CategoryId
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
