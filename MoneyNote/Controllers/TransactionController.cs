0203556a8f3a9e0ee675b64b73ae064582a4cdb3
Diego <diego.sfc100@hotmail.com>
2024-01-24 11:49:18 -0300

    Cria a Controller Transaction

---
+++ MoneyNote/Controllers/TransactionController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MoneyNote.Models;
+
+namespace MoneyNote.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Transaction.Include(t => t.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+        
+        // GET: Transaction/AddOrEdit
+        public IActionResult AddOrEdit()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            return View();
+        }
+
+        // POST: Transaction/AddOrEdit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
+            return View(transaction);
+        }
+                
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transaction == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Transaction'  is null.");
+            }
+            var transaction = await _context.Transaction.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transaction.Remove(transaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }                
+    }
+}
